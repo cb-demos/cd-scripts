3bd307db40626b0d52737b06adb3b775a33ab7fa
Logan Donley <ldonley@cloudbees.com>
2021-11-03 14:51:01 -0400

    GH Changelog summary

--- github/changelog/github-changelog-report.py
+++ github/changelog/github-changelog-report.py
@@ -12,6 +12,8 @@ if not config("REPO"):
 
 repo = config("REPO")
 
+report_path = config("REPORT_PATH", default="")
+
 resp = r.get(f"https://api.github.com/repos/{organization}/{repo}/commits/main")
 json_resp = resp.json()
 
@@ -36,6 +38,16 @@ rendered = template.render(author=f"{json_resp['commit']['author']['name']} <{js
                            date=json_resp['commit']['author']['date'], message=json_resp['commit']['message'],
                            files=files)
 
-
 with open("./github/changelog/changelogs.html", "w") as f:
     f.write(rendered)
+
+mini_template = env.get_template("github-changelog-mini.html")
+
+mini_rendered = mini_template.render(
+    author=f"{json_resp['commit']['author']['name']} <{json_resp['commit']['author']['email']}>",
+    sha=json_resp["sha"], organization=organization, repo=repo,
+    date=json_resp['commit']['author']['date'], message=json_resp['commit']['message'],
+    report_path=report_path)
+
+with open("./github/changelog/mini.html", "w") as f:
+    f.write(mini_rendered)
